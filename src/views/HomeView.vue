19bea29d8d4778863dd3d65183e83510358510e5
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-11-08 22:49:49 -0600

    'installvuetifyandlogin'

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  
+    <h1 class="text-center text-h3 font-weight-bold my-5"> Bienes Raices Vue </h1>
+  
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+</script>
+
+<template>
+  
+    
+
+
+    <v-card
+        flat 
+        max-width="600"
+        class="mx-auto mt-3"
+    >
+        <v-card-title class="text-h4 font-weight-bold" tag="h4">
+          Iniciar Sesion
+        </v-card-title>
+        <v-card-subtitle class="text-h7">
+          Inicia sesion con tu cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+          <v-text-field
+            type="email"
+            label="email"
+            bg-color="black"
+            color="white"
+          />
+        </v-form>
+
+        
+          <v-text-field
+            type="password"
+            label="Password"
+            bg-color="black"
+            color="white"
+          />
+
+          <v-btn
+            block
+            color="orange-lighten-4">
+            Inicia sesión
+          </v-btn>
+      
+    </v-card>
+  
+</template>
